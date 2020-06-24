532fb8e12c6dac3d419d3e5ac6c2a466487209f0
Debanjan-Mondal <49085657+Debanjan-Mondal@users.noreply.github.com>
2020-06-24 19:05:42 +0530

    second commit

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const URL = 'https://api.openweathermap.org/data/2.5/weather';
-const API_KEY = '3d1393770561b76e576aa6da96417dcc';
+const API_KEY = 'YOUR_API_KEY';
 
 export const fetchWeather = async (query) => {
     const { data } = await axios.get(URL, {
